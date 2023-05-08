a2083be8f4c18a177544bd564cdf692a75ad6886
Mateusz Brzezinski <matuxd59@gmail.com>
2023-05-08 06:15:08 +0200

    updated args etc

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,7 +1,19 @@
 const puppeteer = require("puppeteer")
+require("dotenv").config();
 
 const scrapeLogic = async (res) => {
-  const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+          "--disable-setuid-sandbox",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote",
+        ],
+        executablePath:
+          process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),
+      });
   try{
     const { Cluster } = require('puppeteer-cluster');
     const fs = require('fs').promises;
